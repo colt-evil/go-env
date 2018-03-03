a20e77f48a3498ae2a981e79e50901e1aa43741c
Colt Evil <someemail@nothing.org>
2018-03-03 17:35:57 +0800

    update

--- shell-rc-gen.sh
+++ shell-rc-gen.sh
@@ -3,9 +3,6 @@
 
 read -e -p "Type profile file path: " -i ~/".custom_profile" profile_file_path
 
-echo $profile_file_path
-echo ${#profile_file_path}  
-
 read -e -p "Type default env folder: " -i ~/.env ENV
 
 read -e -p "Type Golang install(GOROOT): " -i $ENV/golang GOROOT
@@ -19,8 +16,8 @@ if [ -f $ENV ]; then
 fi
 
 if [ -f $profile_file_path ]; then
-    read -e -p "profile file ${profile_file_path} extists. overwrite it? [Y/N]" -i "N" overwrite
-    if [ "N" == "$overwrite" ]; then
+    read -e -p "profile file ${profile_file_path} extists. overwrite it? [y/n]: " overwrite
+    if [ "n" == "$overwrite" ]; then
         exit
     fi
 else
@@ -29,7 +26,7 @@ fi
 
 
 cat > $profile_file_path <<EOL 
-export GOPATH=${go_path}
+export GOPATH=${GOPATH}
 export PATH=\$PATH:{$GOROOT}/bin:\$GOPATH/bin
 export LANG=en_US.UTF-8
 ORG_GOPATH=\$GOPATH
@@ -47,31 +44,56 @@ function disable_proxy() {
 	curl myip.ipip.net
 }
 
-function set_go_path() {
-	current_path=$(pwd)
-	export GOPATH=$current_path
-	export PATH=$PATH:$GOPATH/bin
-	echo "Current GOPATH: $GOPATH"
+function go_path_set() {
+	current_path=\$1
+	if [ -z "\$current_path" ]; then
+		current_path=\$(pwd)
+	fi
+	export GOPATH=\$current_path
+	export PATH=\$PATH:\$GOPATH/bin
+	echo "Current GOPATH: \$GOPATH"
 }
 
-function unset_go_path() {
-	current_path=$(pwd)
-	export GOPATH=$ORG_GOPATH
-	export PATH=$ORG_PATH
-	echo "Current GOPATH: $GOPATH"
+function go_path_unset() {
+	export GOPATH=\$ORG_GOPATH
+	export PATH=\$ORG_PATH
+	echo "Current GOPATH: \$GOPATH"
+}
+
+function go_path_create() {
+	read -e -p "please enter path: " go_path
+	if [ -d \$go_path ]; then 
+		echo "\$go_path exists, should we continue?[y/n]"
+		read continue
+		if [ "N" == \$continue ]; then
+			exit
+		fi 
+	fi
+	if [ !-d \$go_path/src ]; then
+		mkdir -p \$go_path/src
+	fi
+	go_path_set \$go_path
+	echo "GOPATH \$go_path created"
 }
 EOL
 
-read -e -p "What is your default shell?: 1. bash, 2. zsh :" -i "1" DEFAULT_SHELL
+read -e -p "What is your default shell?: 1. bash, 2. zsh : " DEFAULT_SHELL
 
 shell_profile=~/.bash_profile
 if [ "2" == "DEFAULT_SHELL" ]; then
     shell_profile=~/.zshrc
 fi
 
-echo "Writing load script to $shell_profile"
 
+grep -F "source $profile_file_path" $shell_profile > /dev/null
+not_found=$?
+if [ $not_found -eq 1 ]; then
 cat >> $shell_profile <<EOL 
-    # custom profile
-    source $profile_file_path
+	# custom profile
+	source $profile_file_path
 EOL
+	echo "add load script to $shell_profile"
+else
+	echo "load script already in $shell_profile, auto ignore"
+fi
+
